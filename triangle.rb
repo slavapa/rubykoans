1ae6faa405b41024e3dc6b6bbe031c4581e023c4
slavapa <slavapas13@gmail.com>
2014-06-19 08:42:53 +0300

    Do about_true_and_false and about_triangle_project

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,22 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError, 'Argument is not valid'
+  end
+  
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError, 'Argument is not valid'
+  end
+  
+  if a==b && b==c 
+    :equilateral
+  elsif a==b || b==c || a==c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
